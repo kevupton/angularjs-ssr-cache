18f29908c83f20bc2696b48c71f0f042a9c1c1cd
Kevin Upton <kevin.upton@fairfaxmedia.com.au>
2019-05-03 15:28:16 +1000

    updated version and remove babel builder

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { tap } from 'rxjs/internal/operators/tap';
 import { Engine } from './engine';
 import { startServer } from './server';
-import 'babel-polyfill';
 
 export function run () {
   const engine = new Engine();
 
   return engine.start$.pipe(
-    tap(() => startServer())
+    tap(() => startServer()),
   );
 }
